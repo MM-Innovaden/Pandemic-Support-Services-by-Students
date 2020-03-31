c5b920123caa63d4508affb9def313074d390b90
Sahel <sahel13miqbal@gmail.com>
2020-03-31 14:20:33 +0530

    Debug set to false

--- Corona/settings.py
+++ Corona/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^u&7#!(s+n)cg(#!-@!5f6er5b-&w^jl#*$fmcosbjyc*hiuv7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['134.209.155.42']
 
